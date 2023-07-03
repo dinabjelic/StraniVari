b89ed9de34cb1091e061bd09bfaabdefb64f8e38
Dina Bjelic-Contractor <dbjelic@powerplan.com>
2023-07-03 22:10:59 +0200

    removing insert from controller

--- StraniVari/StraniVari.API/Controllers/SchoolsVolunteersMaterialsController.cs
+++ StraniVari/StraniVari.API/Controllers/SchoolsVolunteersMaterialsController.cs
@@ -1,9 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using StraniVari.Core.Entities;
-using StraniVari.Core.Helper;
+﻿using StraniVari.Core.Entities;
 using StraniVari.Core.Requests;
-using StraniVari.Core.Responses;
 using StraniVari.Services.Interfaces;
 
 namespace StraniVari.API.Controllers
@@ -13,13 +9,5 @@ namespace StraniVari.API.Controllers
         public SchoolsVolunteersMaterialsController(ISchoolsVolunteersMaterialsService  schoolsVolunteersMaterialsService) : base(schoolsVolunteersMaterialsService)
         {
         }
-
-        [Authorize(Roles = Role.Administrator)]
-        public override async Task<IActionResult> Insert(SchoolVolunteerMaterialRequest schoolVolunteerMaterialRequest)
-        {
-            await base.Insert(schoolVolunteerMaterialRequest);
-            return Ok(new ResponseResult { Message = "You succeeded" });
-        }
-
     }
 }
